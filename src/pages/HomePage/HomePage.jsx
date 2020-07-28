30dda967690efd35ce900c2560c0be0cc8a0c636
Grzegorz Kubik <grzeznx2@gmail.com>
2020-07-28 10:08:43 +0200

    add SectionLatestProjects to HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import SectionHero from './Sections/SectionHero'
+import SectionLatestProjects from './Sections/SectionLatestProjects'
 import './HomePage.scss'
 
 export default function HomePage() {
     return (
         <>
             <SectionHero />
+            <SectionLatestProjects />
         </>
     )
 }
---
+++ src/pages/HomePage/Sections/SectionLatestProjects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Button from '../../../components/Button/Button'
+
+export default function SectionLatestProjects() {
+    return (
+        <section class="section-latest-projects">
+            <h2 class="section-title section-title--text-primary">Latest Projects</h2>
+            <div class="container">
+
+                <div class="section-latest-projects__button-wrapper">
+                    <Button to='/invest' modifiers={['primary']}>view all projects</Button>
+                </div>
+            </div>
+        </section>
+    )
+}
