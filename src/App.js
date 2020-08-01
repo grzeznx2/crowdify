2f7871112cf08cc14cd0da6b10232dad11fd275b
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-01 14:05:55 +0200

    add route do AuthPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import HomePage from './pages/HomePage/HomePage'
+import AuthPage from './pages/AuthPage/AuthPage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="header-placeholder"></div>
       <Header />
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/auth' component={AuthPage} />
       <Footer />
     </div>
   );
