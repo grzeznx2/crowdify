200141b14368abf59d1c447ce845d9c8c86d62c6
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-03 17:21:31 +0200

    add isTruthy validator

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -2,6 +2,7 @@ class Validator {
     static minLength = (val, errorMessage) => ({ type: 'MIN_LENGTH', value: val, errorMessage })
     static maxLength = (val, errorMessage) => ({ type: 'MAX_LENGTH', value: val, errorMessage })
     static isRequired = (errorMessage) => ({ type: 'IS_REQUIRED', errorMessage })
+    static isTruthy = (errorMessage) => ({ type: 'IS_TRUTHY', errorMessage })
     static isEmail = (errorMessage) => ({ type: 'IS_EMAIL', errorMessage })
     static isGreaterThanZero = (errorMessage) => ({ type: 'IS_GREATER_THAN_ZERO', errorMessage })
     static areEqual = (value, value2, errorMessage) => ({ type: 'ARE_EQUAL', value, value2, errorMessage })
@@ -11,6 +12,7 @@ class Validator {
         let isValid = true;
 
         if (type === 'IS_REQUIRED') isValid = isValid && inputValue.trim().length > 0
+        if (type === 'IS_TRUTHY') isValid = isValid && inputValue === true
         if (type === 'IS_GREATER_THAN_ZERO') isValid = isValid && typeof inputValue === 'number' && inputValue > 0
         if (type === 'MIN_LENGTH') isValid = isValid && value <= inputValue.trim().length
         if (type === 'MAX_LENGTH') isValid = isValid && value >= inputValue.trim().length
