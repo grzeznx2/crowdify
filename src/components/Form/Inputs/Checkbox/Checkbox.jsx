32cc9182954498ff9ba23d1b71c6d1cd8e4debac
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-03 16:59:38 +0200

    set Checkbox props

--- src/components/Form/Inputs/Checkbox/Checkbox.jsx
+++ src/components/Form/Inputs/Checkbox/Checkbox.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 
+import { joinClasses } from '../../../../utils/utils'
+
 import './Checkbox.scss'
 
-export default function Checkbox() {
+export default function Checkbox({ modifiers, id, name, title }) {
     return (
 
-        <div class="checkbox checkbox--on-light-bg">
-            <input type="checkbox" id="register-agree-terms" class="checkbox__input" />
-            <label htmlFor="register-agree-terms" class="checkbox__label">
+        <div class={joinClasses('checkbox', modifiers)}>
+            <input type="checkbox" id={id} name={name} class="checkbox__input" />
+            <label htmlFor={id} class="checkbox__label">
                 <span class="checkbox__button">
                     <svg class="checkbox__icon">
                         <use xlinkHref="/img/sprite.svg#icon-checkmark" />
                     </svg>
                 </span>
-                                 I agree to the terms
-                              </label>
+                {title}
+            </label>
         </div>
+        // <div class="checkbox checkbox--on-light-bg">
+        //     <input type="checkbox" id="register-agree-terms" class="checkbox__input" />
+        //     <label htmlFor="register-agree-terms" class="checkbox__label">
+        //         <span class="checkbox__button">
+        //             <svg class="checkbox__icon">
+        //                 <use xlinkHref="/img/sprite.svg#icon-checkmark" />
+        //             </svg>
+        //         </span>
+        //                          I agree to the terms
+        //                       </label>
+        // </div>
 
     )
 }
