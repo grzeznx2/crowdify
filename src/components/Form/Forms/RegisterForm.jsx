828be20205f14c3866d49931286104a758fba098
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 07:47:05 +0200

    refator RegisterForm to use Form composition component

--- src/components/Form/Forms/RegisterForm.jsx
+++ src/components/Form/Forms/RegisterForm.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 
-import Button from '../../Button/Button'
-import Checkbox from '../Inputs/Checkbox/Checkbox'
-import InputGroup from '../Inputs/InputGroup/InputGroup'
+import Form from './Form'
 
-import useForm from '../../../hooks/useForm'
-
-import './Form.scss'
-
-export default function RegisterForm() {
-
-    const { inputs, handleChange, handleSubmit } = useForm('register')
+export default function LoginForm() {
 
     return (
-        <form onSubmit={handleSubmit} class="form form--center-column">
-            <h3 class="heading-3 text-blue-20 text-bold form__title">
-                Create Account
-                  </h3>
-            <div class="form__container form__container--auth">
-                {
-                    Object.values(inputs).map(input => {
-                        if (input.type === 'checkbox') return <Checkbox key={input.id} onChange={handleChange} {...input} />
-                        return <InputGroup key={input.id} onChange={handleChange} {...input} />
-                    })
-                }
-            </div>
-            <Button modifiers={['primary']}>Sign Up</Button>
-        </form>
+        <Form name='register' formModifiers={['center-column']} containerModifiers={['auth']} title='create account' buttonText='sign up' children />
     )
 }
