cf422237a0c2bcbeb24f79e17f9c3837c3698252
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 06:05:15 +0200

    render LoginForm inputs dynamically

--- src/components/Form/Forms/LoginForm.jsx
+++ src/components/Form/Forms/LoginForm.jsx
@@ -1,27 +1,95 @@
-import React from 'react'
+import React, { useReducer, useCallback } from 'react'
 
+import Button from '../../Button/Button'
+import Checkbox from '../Inputs/Checkbox/Checkbox'
+import InputGroup from '../Inputs/InputGroup/InputGroup'
 import Validator from '../../../utils/Validator'
 
 import './Form.scss'
 
+const initialState = {
+    loginEmail: {
+        title: 'email',
+        id: 'loginEmail',
+        name: 'loginEmail',
+        type: 'email',
+        value: '',
+        isValid: false,
+        isTouched: false,
+        validators: [Validator.isEmail('Please provide a valid email')],
+        errors: [],
+        modifiers: ['column']
+    },
+    registerPassword: {
+        title: 'password',
+        id: 'registerPassword',
+        name: 'registerPassword',
+        type: 'password',
+        value: '',
+        isValid: false,
+        isTouched: false,
+        validators: [Validator.minLength('Password must contain at least 8 characters')],
+        errors: [],
+        modifiers: ['column']
+    },
+}
+
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_VALUE':
+            const value = action.target.type === "checkbox" ? action.target.checked : action.target.value;
+            const { name } = action.target
+            const { validators } = state[name]
+            const euqalValidator = validators.find(validator => validator.type === 'IS_EQUAL')
+            const equalValue = euqalValidator ? state[euqalValidator.value].value : ''
+            const { isValid, errors } = Validator.validateAll(validators, value, equalValue)
+            return {
+                ...state,
+                [name]: {
+                    ...state[name],
+                    value,
+                    isValid,
+                    errors,
+                    isTouched: true
+                }
+            }
+        default:
+            return state
+    }
+}
+
 export default function LoginForm() {
+    const [inputs, dispatch] = useReducer(formReducer, initialState)
+
+    const handleChange = useCallback(e => {
+        dispatch({ type: 'CHANGE_VALUE', target: e.target })
+    }, [])
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const inputsArray = Object.values(inputs)
+        const isFormValid = inputsArray.every(input => input.isValid)
+        if (isFormValid) {
+            for (let input of inputsArray) {
+                console.log(`${input.name}: ${input.value}`)
+            }
+        }
+    }
     return (
-        <form action="#" class="form form--center-column">
+        <form onSubmit={handleSubmit} class="form form--center-column">
             <h3 class="heading-3 text-blue-20 text-bold form__title">
                 Sign In
-      </h3>
+            </h3>
             <div class="form__container form__container--auth">
-                <div class="input-group input-group--column">
-                    <label htmlFor="sign-in-email" class="input-group__label">Email:</label>
-                    <input id="sign-in-email" type="text" class="input-group__input" />
-                </div>
-                <div class="input-group input-group--column">
-                    <label htmlFor="sign-in-password" class="input-group__label">Password:</label>
-                    <input id="sign-in-password" type="text" class="input-group__input" />
-                </div>
+                {
+                    Object.values(inputs).map(input => {
+                        if (input.type === 'checkbox') return <Checkbox key={input.id} onChange={handleChange} {...input} />
+                        return <InputGroup key={input.id} onChange={handleChange} {...input} />
+                    })
+                }
             </div>
             <a href="#" class="form__forgot-password-link">Forgot your password?</a>
-            <button class="button button--primary">Sign In</button>
+            <Button modifiers={['primary']}>Sign In</Button>
         </form>
     )
 }
