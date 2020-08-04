f8587392b195c69e23141701397e6b5ff0814dc6
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 07:43:45 +0200

    create general Form composition component

---
+++ src/components/Form/Forms/Form.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import Button from '../../Button/Button'
+import Checkbox from '../Inputs/Checkbox/Checkbox'
+import InputGroup from '../Inputs/InputGroup/InputGroup'
+
+import useForm from '../../../hooks/useForm'
+
+import { joinClasses } from '../../../utils/utils'
+
+import './Form.scss'
+
+export default function Form({ name, formModifiers, formOtherClasses, containerModifiers, containerOtherClasses, title, buttonText, children }) {
+
+    const { inputs, handleChange, handleSubmit } = useForm(name)
+
+    const createInputs = inputs => Object.values(inputs).map(input => {
+        if (input.type === 'checkbox') return <Checkbox key={input.id} onChange={handleChange} {...input} />
+        return <InputGroup key={input.id} onChange={handleChange} {...input} />
+    })
+
+    return (
+        <form onSubmit={handleSubmit} className={joinClasses('form', formModifiers, formOtherClasses)}>
+            {
+                title ?
+                    <h3 className="heading-3 text-blue-20 text-bold form__title">
+                        Sign In</h3>
+                    : null
+            }
+            <div className={joinClasses('form__container', containerModifiers, containerOtherClasses)}>
+                {
+                    createInputs(inputs)
+                }
+            </div>
+            {children}
+            <Button modifiers={['primary']}>{buttonText}</Button>
+        </form>
+    )
+}
