a4c8ced7147747a5f530f89af8e35ce4f3ebb49c
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 06:46:24 +0200

    refactor SubscribeForm to use useForm hook

--- src/components/Form/Forms/SubscribeForm.jsx
+++ src/components/Form/Forms/SubscribeForm.jsx
@@ -1,79 +1,14 @@
-import React, { useReducer, useCallback } from 'react'
+import React from 'react'
 
 import Button from '../../Button/Button'
 import InputGroup from '../Inputs/InputGroup/InputGroup'
 
-import Validator from '../../../utils/Validator'
+import useForm from '../../../hooks/useForm'
 
 import './Form.scss'
 
-const initialState = {
-    subscribeName: {
-        title: 'name',
-        id: 'subscribeName',
-        name: 'subscribeName',
-        type: 'text',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isRequired('Name is required')],
-        errors: [],
-        modifiers: ['column']
-    },
-    subscribeEmail: {
-        title: 'email',
-        id: 'subscribeEmail',
-        name: 'subscribeEmail',
-        type: 'email',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isEmail('Please provide valid email')],
-        errors: [],
-        modifiers: ['column']
-    },
-}
-
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE_VALUE':
-            const { name, value } = action.target
-            const { validators } = state[name]
-            const { isValid, errors } = Validator.validateAll(validators, value)
-            return {
-                ...state,
-                [name]: {
-                    ...state[name],
-                    value,
-                    isValid,
-                    errors,
-                    isTouched: true
-                }
-            }
-        default:
-            return state
-    }
-}
-
-
-
 export default function Form() {
-    const [inputs, dispatch] = useReducer(formReducer, initialState)
-
-    const handleChange = useCallback(e => {
-        dispatch({ type: 'CHANGE_VALUE', target: e.target })
-    }, [])
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        const inputsArray = Object.values(inputs)
-        const isFormValid = inputsArray.every(input => input.isValid)
-        if (isFormValid) {
-            for (let input of inputsArray) {
-                console.log(`${input.name}: ${input.value}`)
-            }
-        }
-    }
+    const { inputs, handleChange, handleSubmit } = useForm('subscribe')
 
     return (
         <form onSubmit={handleSubmit} className="form">
