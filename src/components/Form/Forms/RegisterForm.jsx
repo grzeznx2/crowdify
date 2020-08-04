50774aa8036d2dabd40a2f986220ae149d8e47b7
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 06:41:51 +0200

    refactor RegisterForm to use useForm hook

--- src/components/Form/Forms/RegisterForm.jsx
+++ src/components/Form/Forms/RegisterForm.jsx
@@ -1,130 +1,16 @@
-import React, { useReducer, useCallback } from 'react'
+import React from 'react'
 
 import Button from '../../Button/Button'
 import Checkbox from '../Inputs/Checkbox/Checkbox'
 import InputGroup from '../Inputs/InputGroup/InputGroup'
-import Validator from '../../../utils/Validator'
+
+import useForm from '../../../hooks/useForm'
 
 import './Form.scss'
 
-const initialState = {
-    registerFirstName: {
-        title: 'first name',
-        id: 'registerFirstName',
-        name: 'registerFirstName',
-        type: 'text',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isRequired('Name is required')],
-        errors: [],
-        modifiers: ['column']
-    },
-    registerLastName: {
-        title: 'last name',
-        id: 'registerLastName',
-        name: 'registerLastName',
-        type: 'text',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isRequired('Name is required')],
-        errors: [],
-        modifiers: ['column']
-    },
-    registerEmail: {
-        title: 'email',
-        id: 'registerEmail',
-        name: 'registerEmail',
-        type: 'email',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isEmail('Please provide valid email')],
-        errors: [],
-        modifiers: ['column']
-    },
-    registerPassword: {
-        title: 'password',
-        id: 'registerPassword',
-        name: 'registerPassword',
-        type: 'password',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.minLength(8, 'Password must contain at least 8 characters')],
-        errors: [],
-        modifiers: ['column']
-    },
-    registerPasswordConfirm: {
-        title: 'confirm password',
-        id: 'registerPasswordConfirm',
-        name: 'registerPasswordConfirm',
-        type: 'password',
-        value: '',
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.minLength(8, 'Password must contain at least 8 characters'),
-        Validator.isEqual('registerPassword', 'Passwords must be identical')],
-        errors: [],
-        modifiers: ['column']
-    },
-    registerAgreeTerms: {
-        title: 'I agree to the terms',
-        id: 'registerAgreeTerms',
-        name: 'registerAgreeTerms',
-        type: 'checkbox',
-        value: false,
-        isValid: false,
-        isTouched: false,
-        validators: [Validator.isTruthy('You have to agree to the terms to register')],
-        errors: [],
-        modifiers: ['column']
-    },
-}
-
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE_VALUE':
-            const value = action.target.type === "checkbox" ? action.target.checked : action.target.value;
-            const { name } = action.target
-            const { validators } = state[name]
-            const euqalValidator = validators.find(validator => validator.type === 'IS_EQUAL')
-            const equalValue = euqalValidator ? state[euqalValidator.value].value : ''
-            const { isValid, errors } = Validator.validateAll(validators, value, equalValue)
-            return {
-                ...state,
-                [name]: {
-                    ...state[name],
-                    value,
-                    isValid,
-                    errors,
-                    isTouched: true
-                }
-            }
-        default:
-            return state
-    }
-}
-
 export default function RegisterForm() {
 
-    const [inputs, dispatch] = useReducer(formReducer, initialState)
-
-    const handleChange = useCallback(e => {
-        dispatch({ type: 'CHANGE_VALUE', target: e.target })
-    }, [])
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        const inputsArray = Object.values(inputs)
-        const isFormValid = inputsArray.every(input => input.isValid)
-        if (isFormValid) {
-            for (let input of inputsArray) {
-                console.log(`${input.name}: ${input.value}`)
-            }
-        }
-    }
+    const { inputs, handleChange, handleSubmit } = useForm('register')
 
     return (
         <form onSubmit={handleSubmit} class="form form--center-column">
