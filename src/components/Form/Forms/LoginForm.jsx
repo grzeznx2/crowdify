638beda8bd463a75645835c102c3f3ac79e0768f
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 07:44:43 +0200

    refator LoginForm to use Form composition component

--- src/components/Form/Forms/LoginForm.jsx
+++ src/components/Form/Forms/LoginForm.jsx
@@ -1,31 +1,12 @@
 import React from 'react'
 
-import Button from '../../Button/Button'
-import Checkbox from '../Inputs/Checkbox/Checkbox'
-import InputGroup from '../Inputs/InputGroup/InputGroup'
-
-import useForm from '../../../hooks/useForm'
-
-import './Form.scss'
+import Form from './Form'
 
 export default function LoginForm() {
-    const { inputs, handleChange, handleSubmit } = useForm('login')
 
     return (
-        <form onSubmit={handleSubmit} class="form form--center-column">
-            <h3 class="heading-3 text-blue-20 text-bold form__title">
-                Sign In
-            </h3>
-            <div class="form__container form__container--auth">
-                {
-                    Object.values(inputs).map(input => {
-                        if (input.type === 'checkbox') return <Checkbox key={input.id} onChange={handleChange} {...input} />
-                        return <InputGroup key={input.id} onChange={handleChange} {...input} />
-                    })
-                }
-            </div>
+        <Form name='login' formModifiers={['center-column']} containerModifiers={['auth']} title='sign in' buttonText='sign in' children>
             <a href="#" class="form__forgot-password-link">Forgot your password?</a>
-            <Button modifiers={['primary']}>Sign In</Button>
-        </form>
+        </Form>
     )
 }
