b40eaf6347fd25899ef38efba41d38451f46bf07
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 07:59:12 +0200

    refactor SubscribeForm to use Form composition component

--- src/components/Form/Forms/SubscribeForm.jsx
+++ src/components/Form/Forms/SubscribeForm.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
 
-import Button from '../../Button/Button'
-import InputGroup from '../Inputs/InputGroup/InputGroup'
+import Form from './Form'
 
-import useForm from '../../../hooks/useForm'
-
-import './Form.scss'
-
-export default function Form() {
-    const { inputs, handleChange, handleSubmit } = useForm('subscribe')
+export default function LoginForm() {
 
     return (
-        <form onSubmit={handleSubmit} className="form">
-            <div className="form__container form__container--subscribe">
-                {
-                    Object.values(inputs).map(input => <InputGroup onChange={handleChange} key={input.id} {...input} />)
-                }
-            </div>
-            <div className="form__button-wrapper">
-                <Button modifiers={['primary']}>subscribe</Button>
-            </div>
-        </form>
+        <Form name='subscribe' formModifiers={['center-column']} containerModifiers={['subscribe']} buttonText='subscribe' />
     )
 }
