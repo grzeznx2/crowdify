79c9268cd600358cf6be4570728ed73d06bcf50d
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-04 09:08:46 +0200

    modify joinClasses util function to accept strings instead of arrays as arguments

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,19 @@
-export const joinClasses = (root = '', modifiers = [], otherClasses = []) => {
+export const joinClasses = (root = '', modifiers = '', otherClasses = '') => {
+    console.log(modifiers)
+
+    const modifiersArray = modifiers.split(' ')
+    const otherClassesArray = otherClasses.split(' ')
+
     return modifiers ?
-        [root, ...modifiers.map(type => `${root}--${type}`), ...otherClasses].join(' ')
+        [root, ...modifiersArray.map(type => `${root}--${type}`), ...otherClassesArray].join(' ')
         : root ?
-            [root, ...otherClasses].join(' ')
-            : otherClasses.join(' ')
+            [root, ...otherClassesArray].join(' ')
+            : otherClassesArray.join(' ')
 };
+// export const joinClasses = (root = '', modifiers = [], otherClasses = []) => {
+//     return modifiers ?
+//         [root, ...modifiers.map(type => `${root}--${type}`), ...otherClasses].join(' ')
+//         : root ?
+//             [root, ...otherClasses].join(' ')
+//             : otherClasses.join(' ')
+// };
