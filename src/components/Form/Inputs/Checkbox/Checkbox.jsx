0ffb80b49f5b622683e3f50d0973926109fd59d1
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-05 08:05:14 +0200

    replace 'class' with 'className'

--- src/components/Form/Inputs/Checkbox/Checkbox.jsx
+++ src/components/Form/Inputs/Checkbox/Checkbox.jsx
@@ -10,10 +10,10 @@ import './Checkbox.scss'
 export default React.memo(function Checkbox({ onChange, modifiers, otherClasses, id, name, title, isValid, isTouched, errors, value }) {
     return (
 
-        <div key={id} class={joinClasses('checkbox', modifiers, otherClasses)}>
-            <input type="checkbox" checked={value} id={id} name={name} class="checkbox__input" onChange={onChange} />
-            <label htmlFor={id} class="checkbox__label">
-                <span class="checkbox__button">
+        <div key={id} className={joinClasses('checkbox', modifiers, otherClasses)}>
+            <input type="checkbox" checked={value} id={id} name={name} className="checkbox__input" onChange={onChange} />
+            <label htmlFor={id} className="checkbox__label">
+                <span className="checkbox__button">
                     <SvgIcon root='checkbox__icon' svgId='icon-checkmark' />
                 </span>
                 {title}
