87eb03b0990b028b96cc18a49c72fd52ae406f41
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-05 08:47:25 +0200

    add min, max and step props

--- src/components/Form/Inputs/Input/Input.jsx
+++ src/components/Form/Inputs/Input/Input.jsx
@@ -6,15 +6,28 @@ import './Input.scss'
 
 import { joinClasses } from '../../../../utils/utils'
 
-export default React.memo(function InputGroup({ onChange, id, name, type, isValid, isTouched, value, modifiers, otherClasses, title, errors }) {
+export default React.memo(function InputGroup({ onChange, id, name, type, isValid, isTouched, value, modifiers, otherClasses, title, min, max, step, errors }) {
     let validityModifier = isTouched ?
         isValid ? 'valid' : 'invalid'
         : ''
 
     return (
-        <div key={id} className={joinClasses('input', modifiers, otherClasses)}>
-            <label htmlFor={id} className="input__label">{title}:</label>
-            <input value={value} id={id} name={name} type={type} onChange={onChange} className={joinClasses('input__input', validityModifier)} />
+        <div
+            key={id}
+            className={joinClasses('input', modifiers, otherClasses)}>
+            <label
+                htmlFor={id}
+                className="input__label">{title}:</label>
+            <input
+                value={value}
+                id={id}
+                name={name}
+                type={type}
+                onChange={onChange}
+                min={min}
+                max={max}
+                step={step}
+                className={joinClasses('input__input', validityModifier)} />
             {
                 errors.map(error => <FormError message={error} />)
             }
