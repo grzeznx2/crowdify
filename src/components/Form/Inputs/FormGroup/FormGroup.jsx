d02ff39b26b6f84ec631e8a981c3fee46c8c7ee2
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-05 07:42:07 +0200

    add FormGroup component

---
+++ src/components/Form/Inputs/FormGroup/FormGroup.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import Checkbox from '../Checkbox/Checkbox'
+import InputGroup from '../InputGroup/InputGroup'
+import RangeSlider from '../RangeSlider/RangeSlider'
+
+export default function FormGroup(props) {
+
+    let markup = null
+
+    switch (props.type) {
+        case 'text':
+        case 'select':
+        case 'email':
+        case 'password':
+            markup = <InputGroup {...props} />
+            break;
+        case 'checkbox':
+            markup = <Checkbox {...props} />
+            break;
+        case 'range':
+            markup = <RangeSlider {...props} />
+            break;
+        default:
+            return null
+    }
+
+    return (
+        <div className="form-group">
+            {
+                markup
+            }
+        </div>
+    )
+}
