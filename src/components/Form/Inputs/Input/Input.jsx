19d18ffbc2003ca4ba550e8d3505290f5697c241
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-05 09:15:21 +0200

    enable select type and render select options dynamically

--- src/components/Form/Inputs/Input/Input.jsx
+++ src/components/Form/Inputs/Input/Input.jsx
@@ -6,10 +6,42 @@ import './Input.scss'
 
 import { joinClasses } from '../../../../utils/utils'
 
-export default React.memo(function InputGroup({ onChange, id, name, type, isValid, isTouched, value, modifiers, otherClasses, title, min, max, step, errors }) {
+export default React.memo(function InputGroup({ onChange, id, name, type, isValid, isTouched, value, modifiers, otherClasses, title, min, max, options, step, errors }) {
     let validityModifier = isTouched ?
         isValid ? 'valid' : 'invalid'
         : ''
+
+    let inputMarkup
+
+    if (type === 'select') {
+        inputMarkup = <select
+            value={value}
+            id={id}
+            name={name}
+            type={type}
+            onChange={onChange}
+            className={joinClasses('input__input', validityModifier)}>
+            {
+                Object.keys(options).map(option => {
+                    const value = option
+                    const text = options[option]
+                    return <option value={value}>{text}</option>
+                })
+            }
+        </select>
+
+    } else {
+        inputMarkup = <input
+            value={value}
+            id={id}
+            name={name}
+            type={type}
+            onChange={onChange}
+            min={min}
+            max={max}
+            step={step}
+            className={joinClasses('input__input', validityModifier)} />
+    }
 
     return (
         <div
@@ -18,16 +50,9 @@ export default React.memo(function InputGroup({ onChange, id, name, type, isVali
             <label
                 htmlFor={id}
                 className="input__label">{title}:</label>
-            <input
-                value={value}
-                id={id}
-                name={name}
-                type={type}
-                onChange={onChange}
-                min={min}
-                max={max}
-                step={step}
-                className={joinClasses('input__input', validityModifier)} />
+            {
+                inputMarkup
+            }
             {
                 errors.map(error => <FormError message={error} />)
             }
