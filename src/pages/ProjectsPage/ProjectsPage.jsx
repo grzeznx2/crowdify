017098e6e6c5725613559fc3958cbb2543b14ccf
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-07 07:49:35 +0200

    separate Pagination component from ProjectsPage

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import useFetch from '../../hooks/useFetch'
 
 import Filtration from '../../components/Filtration/Filtration'
+import Pagination from '../../components/Pagination/Pagination'
 import Project from '../../components/Project/Project'
 
 import './ProjectsPage.scss'
@@ -55,28 +56,7 @@ export default function ProjectsPage() {
                     }
                 </div>
             </div>
-            <div className="pagination">
-                <a href="#" className="pagination__button pagination__button--arrow">
-                    <svg className="pagination__icon">
-                        <use xlinkHref='/img/sprite.svg#icon-chevron-left' />
-                    </svg>
-                </a>
-                <a href="#" className="pagination__button">1</a>
-                <a href="#" className="pagination__button">2</a>
-                <a href="#" className="pagination__button">3</a>
-                <a href="#" className="pagination__button">4</a>
-                <a href="#" className="pagination__button">5</a>
-                <a href="#" className="pagination__button">6</a>
-                <a href="#" className="pagination__button">7</a>
-                <a href="#" className="pagination__button">8</a>
-                <a href="#" className="pagination__button pagination__button--active">9</a>
-                <a href="#" className="pagination__button">10</a>
-                <a href="#" className="pagination__button">
-                    <svg className="pagination__icon">
-                        <use xlinkHref='/img/sprite.svg#icon-chevron-right' />
-                    </svg>
-                </a>
-            </div>
+            <Pagination />
         </section>
     )
 }
