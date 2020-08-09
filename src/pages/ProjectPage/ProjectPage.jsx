1a02cd79a7880a1acf513021aeae02d72d36d942
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-09 11:23:55 +0200

    add fetching project functionality

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
+import useFetch from '../../hooks/useFetch'
+
+import Loader from '../../components/Loader/Loader'
 import LoanDetails from './Sections/LoanDetails/LoanDetails'
 import ProjectDescription from './Sections/ProjectDescription/ProjectDescription'
 import ProjectPresentation from './Sections/ProjectPresentation/ProjectPresentation'
@@ -7,10 +11,40 @@ import ProjectPresentation from './Sections/ProjectPresentation/ProjectPresentat
 import './ProjectPage.scss'
 
 export default function ProjectPage() {
+    const [project, setProject] = useState({})
+    const { isLoading, error, sendRequest } = useFetch()
+    const { projectId } = useParams()
+
+    useEffect(() => {
+
+        const fetchProject = async () => {
+            const options = {
+                url: `http://localhost:5000/api/v1/projectss/${projectId}`
+            }
+            const response = await sendRequest(options)
+            if (response) {
+                console.log(response)
+                setProject(response.project)
+            }
+
+        }
+
+        fetchProject()
+
+    }, [projectId])
+
+
     return (<>
-        <ProjectPresentation />
-        <LoanDetails />
-        <ProjectDescription />
+        <Loader
+            isLoading={isLoading}
+            loadingComp='dots'
+            error={error}
+        >
+            <ProjectPresentation />
+            <LoanDetails />
+            <ProjectDescription />
+        </Loader>
+
     </>
     )
 }
