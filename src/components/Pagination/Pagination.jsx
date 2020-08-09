e0512c3d893ad63d4b9d9d4cf1562e39c74f88e3
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-09 06:28:49 +0200

    add arrow button functionality

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
+import Button from '../Button/Button'
 import SvgIcon from '../SvgIcon/SvgIcon'
-
-import { joinClasses } from '../../utils/utils'
 
 import './Pagination.scss'
 
@@ -11,17 +10,25 @@ export default function Pagination({ changePage, resultsCount, resPerPage, page 
 
     return (
         <div className="pagination">
-            <a href="#" className="pagination__button pagination__button--arrow">
-                <SvgIcon root="pagination__icon" svgId='icon-chevron-left' />
-            </a>
+            {
+                page !== 1 &&
+                <Button root='pagination__button' handleClick={() => changePage(page - 1)}>
+                    <SvgIcon root="pagination__icon" svgId='icon-chevron-left' />
+                </Button>
+            }
+
             {
                 [...Array(pagesCount)].map((_, i) => {
                     return <button onClick={() => changePage(i + 1)} className={`pagination__button${page === i + 1 ? ' pagination__button--active' : ''}`}>{i + 1}</button>
                 })
             }
-            <a href="#" className="pagination__button">
-                <SvgIcon root="pagination__icon" svgId='icon-chevron-right' />
-            </a>
+            {
+                page !== pagesCount &&
+                <Button root='pagination__button' handleClick={() => changePage(page + 1)}>
+                    <SvgIcon root="pagination__icon" svgId='icon-chevron-right' />
+                </Button>
+            }
+
         </div>
     )
 }
