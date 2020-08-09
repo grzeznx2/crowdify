f0aa4dc39f6ee1649bfb6f1bc9d9e5c3d870b0bc
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-09 09:06:29 +0200

    refactor to use 'useForm' correctly

--- src/components/Form/Forms/Form.jsx
+++ src/components/Form/Forms/Form.jsx
@@ -11,23 +11,23 @@ import './Form.scss'
 
 export default function Form({ name, formModifiers, formOtherClasses, containerModifiers, containerOtherClasses, title, buttonText, children }) {
 
-    const { inputs, fetchState, handleChange, handleSubmit } = useForm(name)
+    const { inputs, isLoading, error, handleChange, handleSubmit } = useForm(name)
 
-    return (null
-        // <form onSubmit={handleSubmit} className={joinClasses('form', formModifiers, formOtherClasses)}>
-        //     {
-        //         title ?
-        //             <h3 className="heading-3 text-blue-20 text-bold form__title">
-        //                 Sign In</h3>
-        //             : null
-        //     }
-        //     <div className={joinClasses('form__container', containerModifiers, containerOtherClasses)}>
-        //         {
-        //             Object.values(inputs).map(input => <FormGroup key={input.id} onChange={handleChange} {...input} />)
-        //         }
-        //     </div>
-        //     {children}
-        //     <Button modifiers='primary'>{fetchState.isLoading ? 'Sending...' : buttonText}</Button>
-        // </form>
+    return (
+        <form onSubmit={handleSubmit} className={joinClasses('form', formModifiers, formOtherClasses)}>
+            {
+                title ?
+                    <h3 className="heading-3 text-blue-20 text-bold form__title">
+                        Sign In</h3>
+                    : null
+            }
+            <div className={joinClasses('form__container', containerModifiers, containerOtherClasses)}>
+                {
+                    Object.values(inputs).map(input => <FormGroup key={input.id} onChange={handleChange} {...input} />)
+                }
+            </div>
+            {children}
+            <Button modifiers='primary'>{isLoading ? 'Sending...' : buttonText}</Button>
+        </form>
     )
 }
