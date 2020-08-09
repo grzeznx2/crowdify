e27ee3d129530465faad8e6e549809c3bf5eb708
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-09 07:16:48 +0200

    improve arrow buttos conditional rendering

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -18,12 +18,13 @@ export default function Pagination({ changePage, resultsCount, resPerPage, page 
             }
 
             {
+                pagesCount > 1 &&
                 [...Array(pagesCount)].map((_, i) => {
                     return <button onClick={() => changePage(i + 1)} className={`pagination__button${page === i + 1 ? ' pagination__button--active' : ''}`}>{i + 1}</button>
                 })
             }
             {
-                page !== pagesCount &&
+                page !== pagesCount && pagesCount > 1 &&
                 <Button root='pagination__button' handleClick={() => changePage(page + 1)}>
                     <SvgIcon root="pagination__icon" svgId='icon-chevron-right' />
                 </Button>
