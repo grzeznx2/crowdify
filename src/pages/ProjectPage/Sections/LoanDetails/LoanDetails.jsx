ef79c16796e818a8b22dba8e47feddb2371a76a0
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-10 06:58:51 +0200

    render dynamic values

--- src/pages/ProjectPage/Sections/LoanDetails/LoanDetails.jsx
+++ src/pages/ProjectPage/Sections/LoanDetails/LoanDetails.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
+import React from 'react';
 
-import DataBox from '../../../../components/DataBox/DataBox'
+import DataBox from '../../../../components/DataBox/DataBox';
 
-export default function LoanDetails() {
-    return (
-        <section class="section-loan-details">
-            <h2 class="section-title section-title--text-white">
-                Loan Details
-           </h2>
-            <div class="container">
-                <div class="section-loan-details__details-container">
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='minimal target' value='25, 000 $' />
-                    </div>
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='total target' value='250, 000 $' />
-                    </div>
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='interest rate' value='26%' />
-                    </div>
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='loan period' value='6 months' />
-                    </div>
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='interest payment' value='Every 3 months' />
-                    </div>
-                    <div class="section-loan-details__detail">
-                        <DataBox modifiers='bg-blue' title='buyBack guarantee' value='Yes' />
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+export default function LoanDetails({
+  minTarget,
+  totalTarget,
+  interestRate,
+  duration,
+  interestPayments,
+}) {
+  return (
+    <section class="section-loan-details">
+      <h2 class="section-title section-title--text-white">Loan Details</h2>
+      <div class="container">
+        <div class="section-loan-details__details-container">
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="minimal target"
+              value={`${minTarget} $`}
+            />
+          </div>
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="total target"
+              value={`${totalTarget} $`}
+            />
+          </div>
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="interest rate"
+              value={`${interestRate}%`}
+            />
+          </div>
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="loan period"
+              value={`${duration} ${duration === 1 ? 'month' : 'months'}`}
+            />
+          </div>
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="interest payment"
+              value={interestPayments}
+            />
+          </div>
+          <div class="section-loan-details__detail">
+            <DataBox
+              modifiers="bg-blue"
+              title="buyBack guarantee"
+              value="Yes"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
