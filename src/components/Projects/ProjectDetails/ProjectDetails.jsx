0a602597f4661ac4de5b4905d4b3899f30bd910a
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-10 06:35:26 +0200

    add class props

--- src/components/Projects/ProjectDetails/ProjectDetails.jsx
+++ src/components/Projects/ProjectDetails/ProjectDetails.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-import './ProjectDetails.scss'
+import { joinClasses } from '../../../utils/utils';
 
-export default function ProjectDetails({ interestRate, type, duration, location }) {
-    return (
-        <div class="project-details">
-            <ul class="project-details__list">
-                <li class="project-details__item">
-                    <span class="project-details__label">interest rate:</span>
-                    <span class="project-details__value">{interestRate}%</span>
-                </li>
-                <li class="project-details__item">
-                    <span class="project-details__label">type:</span>
-                    <span class="project-details__value">{type}</span>
-                </li>
-                <li class="project-details__item">
-                    <span class="project-details__label">period:</span>
-                    <span class="project-details__value">{duration} months</span>
-                </li>
-                <li class="project-details__item">
-                    <span class="project-details__label">location:</span>
-                    <span class="project-details__value">{location}</span>
-                </li>
-            </ul>
-        </div>
+import './ProjectDetails.scss';
 
-    )
+export default function ProjectDetails({
+  interestRate,
+  type,
+  duration,
+  location,
+  modifiers,
+  otherClasses,
+}) {
+  return (
+    <div className={joinClasses('project-details', modifiers, otherClasses)}>
+      <ul className="project-details__list">
+        <li className="project-details__item">
+          <span className="project-details__label">interest rate:</span>
+          <span className="project-details__value">{interestRate}%</span>
+        </li>
+        <li className="project-details__item">
+          <span className="project-details__label">type:</span>
+          <span className="project-details__value">{type}</span>
+        </li>
+        <li className="project-details__item">
+          <span className="project-details__label">period:</span>
+          <span className="project-details__value">{duration} months</span>
+        </li>
+        <li className="project-details__item">
+          <span className="project-details__label">location:</span>
+          <span className="project-details__value">{location}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
