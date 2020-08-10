cddbe20d7914e4b1d8576748013b0f638a212c09
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-10 06:35:53 +0200

    pass missing props to ProjectPresentation

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,78 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import useFetch from '../../hooks/useFetch'
+import useFetch from '../../hooks/useFetch';
 
-import Loader from '../../components/Loader/Loader'
-import LoanDetails from './Sections/LoanDetails/LoanDetails'
-import ProjectDescription from './Sections/ProjectDescription/ProjectDescription'
-import ProjectPresentation from './Sections/ProjectPresentation/ProjectPresentation'
+import Loader from '../../components/Loader/Loader';
+import LoanDetails from './Sections/LoanDetails/LoanDetails';
+import ProjectDescription from './Sections/ProjectDescription/ProjectDescription';
+import ProjectPresentation from './Sections/ProjectPresentation/ProjectPresentation';
 
-import './ProjectPage.scss'
+import './ProjectPage.scss';
 
 export default function ProjectPage() {
-    const [project, setProject] = useState({})
-    const { isLoading, error, sendRequest } = useFetch()
-    const { projectId } = useParams()
+  const [project, setProject] = useState({});
+  const { isLoading, error, sendRequest } = useFetch();
+  const { projectId } = useParams();
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchProject = async () => {
+      const options = {
+        url: `http://localhost:5000/api/v1/projects/${projectId}`,
+      };
+      const response = await sendRequest(options);
+      if (response) {
+        console.log(response);
+        setProject(response.project);
+      }
+    };
 
-        const fetchProject = async () => {
-            const options = {
-                url: `http://localhost:5000/api/v1/projects/${projectId}`
-            }
-            const response = await sendRequest(options)
-            if (response) {
-                console.log(response)
-                setProject(response.project)
-            }
+    fetchProject();
+  }, [projectId]);
 
-        }
+  const {
+    comments,
+    createdAt,
+    duration,
+    endDate,
+    imageUrl,
+    interestPayments,
+    interestPaymentsRate,
+    interestPaymentsStart,
+    interestRate,
+    interestsDates,
+    interestsNumber,
+    investors,
+    latestComments,
+    location,
+    minTarget,
+    name,
+    paid,
+    startDate,
+    status,
+    summary,
+    totalTarget,
+    type,
+  } = project;
 
-        fetchProject()
-
-    }, [projectId])
-
-    const {
-        comments,
-        createdAt,
-        duration,
-        endDate,
-        imageUrl,
-        interestPayments,
-        interestPaymentsRate,
-        interestPaymentsStart,
-        interestRate,
-        interestsDates,
-        interestsNumber,
-        investors,
-        latestComments,
-        location,
-        minTarget,
-        name,
-        paid,
-        startDate,
-        status,
-        summary,
-        totalTarget,
-        type } = project
-
-    return (<>
-        <Loader
-            isLoading={isLoading}
-            loadingComp='dots'
-            error={error}
-        >
-            <ProjectPresentation
-                location={location}
-                name={name}
-                paid={paid}
-                minTarget={minTarget}
-                totalTarget={totalTarget} />
-            <LoanDetails />
-            <ProjectDescription />
-        </Loader>
-
+  return (
+    <>
+      <Loader isLoading={isLoading} loadingComp="dots" error={error}>
+        <ProjectPresentation
+          duration={duration}
+          imageUrl={imageUrl}
+          interestRate={interestRate}
+          location={location}
+          name={name}
+          paid={paid}
+          minTarget={minTarget}
+          totalTarget={totalTarget}
+          type={type}
+        />
+        <LoanDetails />
+        <ProjectDescription />
+      </Loader>
     </>
-    )
+  );
 }
