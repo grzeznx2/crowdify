80e823bc8649508c5ad2ffb23551b7d84261e0ff
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-10 12:00:46 +0200

    add temporary interests rendering

--- src/pages/ProjectPage/Sections/ProjectDescription/RepaymentPlan/RepaymentPlan.jsx
+++ src/pages/ProjectPage/Sections/ProjectDescription/RepaymentPlan/RepaymentPlan.jsx
@@ -1,22 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import FormGroup from '../../../../../components/Form/Inputs/FormGroup/FormGroup';
 
 import './RepaymentPlan.scss';
 
 export default function RepaymentPlan({
+  duration,
   interestPaymentsStart,
   interestPaymentsRate,
   interestRate,
-  interestDates,
+  interestsDates,
   interestsNumber,
   endDate,
 }) {
+  const [value, setValue] = useState(1000);
+
+  const calcInterestsDistribution = (interestPaymentsStart, interestPaymentsRate, duration) => {
+    const interestsDistribution = {};
+    interestsDistribution.first = interestPaymentsStart / duration;
+    interestsDistribution.remaining = interestPaymentsRate / duration;
+
+    return interestsDistribution;
+  }
+
+  const calcInterestAmount = (value, interestRate, duration) => {
+    return (value * (interestRate / 100) * (duration / 12)).toFixed(2) * 1;
+  };
+
+  const createInterests = (interestsDistribution, interestsDates, interestAmount) => {
+    return interestsDates.map((date, i) => ({
+      date,
+      amount: i === 0 ?
+        (interestAmount * interestsDistribution.first).toFixed(2) * 1
+        : (interestAmount * interestsDistribution.remaining).toFixed(2) * 1,
+    }));
+  };
+
+  const createPlan = () => {
+    const interestAmount = calcInterestAmount(value, interestRate, duration);
+    console.log(interestAmount);
+
+    const interestsDistribution = calcInterestsDistribution(interestPaymentsStart, interestPaymentsRate, duration)
+
+    const interests = createInterests(interestsDistribution, interestsDates, interestAmount)
+    // console.log(interests)
+
+    return interests
+  };
+
+  const changeValue = (e) => {
+    const { value } = e.target;
+    setValue(value);
+  };
+  // return ''
   return (
     <aside class="repayment-plan">
-      <span class="repayment-plan__title">Sample Repayment Plan</span>
+      <span class="repayment-plan__title">{duration}</span>
       <div className="repayment-plan__input">
         <FormGroup
+          value={value}
+          onChange={changeValue}
           type="number"
           min={0}
           title="amount invested"
@@ -27,7 +70,55 @@ export default function RepaymentPlan({
         />
       </div>
       <div class="repayment-plan__infographic">
-        <div class="repayment-plan__infographic-group">
+        {
+          [endDate, ...createPlan()].map((interest, i) => {
+            if (i === 0)
+              return <div class="repayment-plan__infographic-group">
+                <div class="repayment-plan__node">
+                  <span class="repayment-plan__date">{interest.split('T')[0]}</span>
+                  <div class="repayment-plan__circle-placeholder"></div>
+                  <svg
+                    viewBox="0 0 40 40"
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="repayment-plan__circle-container"
+                  >
+                    <circle cx="20" cy="20" r="17" class="repayment-plan__circle" />
+                  </svg>
+                  <span class="repayment-plan__interest">Start Date</span>
+                </div>
+              </div>
+            return <div class="repayment-plan__infographic-group">
+              <div class="repayment-plan__bridge-container">
+                <svg
+                  viewBox="0 0 10 102"
+                  class="repayment-plan__bridge"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <line
+                    x1="5"
+                    y1="102"
+                    x2="5"
+                    y2="0"
+                    class="repayment-plan__line"
+                  />
+                </svg>
+              </div>
+              <div class="repayment-plan__node">
+                <span class="repayment-plan__date">{interest.date.split('T')[0]}</span>
+                <div class="repayment-plan__circle-placeholder"></div>
+                <svg
+                  viewBox="0 0 40 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="repayment-plan__circle-container"
+                >
+                  <circle cx="20" cy="20" r="17" class="repayment-plan__circle" />
+                </svg>
+                <span class="repayment-plan__interest">{interest.amount}$</span>
+              </div>
+            </div>
+          })
+        }
+        {/* <div class="repayment-plan__infographic-group">
           <div class="repayment-plan__node">
             <span class="repayment-plan__date">31.07.2020</span>
             <div class="repayment-plan__circle-placeholder"></div>
@@ -127,7 +218,7 @@ export default function RepaymentPlan({
             </svg>
             <span class="repayment-plan__interest">24.5$</span>
           </div>
-        </div>
+        </div> */}
       </div>
       <div class="repayment-plan__total">
         Total repayment:{' '}
