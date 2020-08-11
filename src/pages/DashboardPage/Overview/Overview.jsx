3cd2e58275095fc187921575931a4d6d89b05102
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-11 10:14:27 +0200

    add Overview

---
+++ src/pages/DashboardPage/Overview/Overview.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export default function Overview() {
+    return (
+        <>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+            <div class="section-dashboard__box section-dashboard__box--small">
+                <div class="data-box data-box--bg-white">
+                    <div class="data-box__title-box">
+                        <h5 class="heading-5 data-box__title">Minimal Target</h5>
+                    </div>
+                    <div class="data-box__content-box">
+                        <span class="data-box__value">25, 000 $</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
