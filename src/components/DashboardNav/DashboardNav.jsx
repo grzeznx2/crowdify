3b23fa1e8b69c17c8de770a22130f2b53f4d1f7b
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-11 10:48:21 +0200

    use NavLinks

--- src/components/DashboardNav/DashboardNav.jsx
+++ src/components/DashboardNav/DashboardNav.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
+import { NavLink, useRouteMatch } from 'react-router-dom'
 
 import './DashboardNav.scss'
 
 export default function DashboardNav() {
+    const match = useRouteMatch()
+
     return (
-        <nav class="dashboard-nav">
-            <ul class="dashboard-nav__list">
-                <li class="dashboard-nav__item">
-                    <a href="#" class="dashboard-nav__link dashboard-nav__link--active">Overview</a>
+        <nav className="dashboard-nav">
+            <ul className="dashboard-nav__list">
+                <li className="dashboard-nav__item">
+                    <NavLink to={`${match.url}/overview`} activeClassName='dashboard-nav__link--active' className="dashboard-nav__link">Overview</NavLink>
                 </li>
-                <li class="dashboard-nav__item">
-                    <a href="#" class="dashboard-nav__link">Transactions</a>
+                <li className="dashboard-nav__item">
+                    <NavLink to={`${match.url}/transactions`} activeClassName='' className="dashboard-nav__link">Transactions</NavLink>
                 </li>
-                <li class="dashboard-nav__item">
-                    <a href="#" class="dashboard-nav__link">Investments</a>
+                <li className="dashboard-nav__item">
+                    <NavLink to={`${match.url}/investments`} activeClassName='' className="dashboard-nav__link">Investments</NavLink>
                 </li>
-                <li class="dashboard-nav__item">
-                    <a href="#" class="dashboard-nav__link">Profile</a>
+                <li className="dashboard-nav__item">
+                    <NavLink to={`${match.url}/profile`} activeClassName='' className="dashboard-nav__link">Profile</NavLink>
                 </li>
-                <li class="dashboard-nav__item">
-                    <a href="#" class="dashboard-nav__link">Contact Us</a>
+                <li className="dashboard-nav__item">
+                    <NavLink to={`${match.url}/contact`} activeClassName='' className="dashboard-nav__link">Contact Us</NavLink>
                 </li>
             </ul>
         </nav>
