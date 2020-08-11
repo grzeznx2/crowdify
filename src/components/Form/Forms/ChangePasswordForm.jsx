8a1b07a8675f808ca3ff0c9e4565e2e038f3235f
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-11 11:20:41 +0200

    add ChangePasswordForm

---
+++ src/components/Form/Forms/ChangePasswordForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Form from './Form'
+
+export default function ChangePasswordForm() {
+
+    return (
+        <Form
+
+            name='changePassword'
+            containerModifiers='change-password'
+            buttonWrapperModifiers='change-password'
+            buttonText='save password'>
+        </Form>
+    )
+}
