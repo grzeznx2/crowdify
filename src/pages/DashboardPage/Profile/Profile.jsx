c4c0dc34fa23af8bd3dbe478a7565dee13fde444
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-12 08:37:46 +0200

    use FormGroup components

--- src/pages/DashboardPage/Profile/Profile.jsx
+++ src/pages/DashboardPage/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import FormGroup from '../../../components/Form/Inputs/FormGroup/FormGroup'
 import RoundButton from '../../../components/Button/RoundButton/RoundButton'
 import ChangePasswordForm from '../../../components/Form/Forms/ChangePasswordForm'
 
@@ -27,43 +28,42 @@ export default function Profile() {
                     <div className="data-box__content-box">
                         <form action="#" className="form">
                             <div className="form__container form__container--change-personal-data">
-                                <div className="input-group input-group--change-personal-data">
-                                    <label for="change-personal-data-first-name" className="input-group__label">First
-											 name:</label>
-                                    <input type="text" id='change-personal-data-first-name'
-                                        className="input-group__input input-group__input--invalid" />
-                                    <div className="input-group__round-buttons-container">
-                                        <RoundButton modifiers='edit' />
-                                    </div>
-                                </div>
-                                <div className="input-group input-group--change-personal-data">
-                                    <label for="change-personal-data-first-name"
-                                        className="input-group__label">Email:</label>
-                                    <input type="text" id='change-personal-data-first-name'
-                                        className="input-group__input input-group__input--valid" />
-                                    <div className="input-group__round-buttons-container">
-                                        <RoundButton modifiers='edit' />
-                                    </div>
-                                </div>
-                                <div className="input-group input-group--change-personal-data">
-                                    <label for="change-personal-data-first-name"
-                                        className="input-group__label">Address:</label>
-                                    <input type="text" id='change-personal-data-first-name'
-                                        className="input-group__input input-group__input" />
-                                    <div className="input-group__round-buttons-container">
-                                        <RoundButton modifiers='abort' />
-                                        <RoundButton modifiers='accept' />
-                                    </div>
-                                </div>
-                                <div className="input-group input-group--change-personal-data">
-                                    <label for="change-personal-data-first-name"
-                                        className="input-group__label">Country:</label>
-                                    <input type="text" id='change-personal-data-first-name'
-                                        className="input-group__input input-group__input--invalid" />
-                                    <div className="input-group__round-buttons-container">
-                                        <RoundButton modifiers='edit' />
-                                    </div>
-                                </div>
+                                <FormGroup
+                                    modifiers='change-personal-data'
+                                    formGroupModifiers='change-personal-data'
+                                    id='changePersonalDataFirstName'
+                                    name='changePersonalDataFirstName'
+                                    type='text'
+                                    title='first name'
+                                    hasRoundButtons={true}
+                                />
+                                <FormGroup
+                                    modifiers='change-personal-data'
+                                    formGroupModifiers='change-personal-data'
+                                    id='changePersonalDataEmail'
+                                    name='changePersonalDataEmail'
+                                    type='email'
+                                    title='email'
+                                    hasRoundButtons={true}
+                                />
+                                <FormGroup
+                                    modifiers='change-personal-data'
+                                    formGroupModifiers='change-personal-data'
+                                    id='changePersonalDataCountry'
+                                    name='changePersonalDataCountry'
+                                    type='text'
+                                    title='country'
+                                    hasRoundButtons={true}
+                                />
+                                <FormGroup
+                                    modifiers='change-personal-data'
+                                    formGroupModifiers='change-personal-data'
+                                    id='changePersonalDataAddress'
+                                    name='changePersonalDataAddress'
+                                    type='text'
+                                    title='address'
+                                    hasRoundButtons={true}
+                                />
                             </div>
                         </form>
                     </div>
