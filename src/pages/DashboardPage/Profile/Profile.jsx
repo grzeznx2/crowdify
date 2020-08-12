8b07b1deb82b0bc4404f85455662b50e6f0355d9
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-12 06:38:48 +0200

    replace 'class' with 'className'

--- src/pages/DashboardPage/Profile/Profile.jsx
+++ src/pages/DashboardPage/Profile/Profile.jsx
@@ -5,75 +5,75 @@ import ChangePasswordForm from '../../../components/Form/Forms/ChangePasswordFor
 export default function Profile() {
     return (
         <>
-            <div class="section-dashboard__box">
-                <div class="data-box data-box--bg-white">
-                    <div class="data-box__title-box">
-                        <h5 class="heading-5 data-box__title data-box__title--text-left">Password
+            <div className="section-dashboard__box">
+                <div className="data-box data-box--bg-white">
+                    <div className="data-box__title-box">
+                        <h5 className="heading-5 data-box__title data-box__title--text-left">Password
 								 </h5>
                     </div>
-                    <div class="data-box__content-box">
+                    <div className="data-box__content-box">
                         <ChangePasswordForm />
                     </div>
                 </div>
             </div>
-            <div class="section-dashboard__box">
-                <div class="data-box data-box--bg-white">
-                    <div class="data-box__title-box">
-                        <h5 class="heading-5 data-box__title data-box__title--text-left">Personal
+            <div className="section-dashboard__box">
+                <div className="data-box data-box--bg-white">
+                    <div className="data-box__title-box">
+                        <h5 className="heading-5 data-box__title data-box__title--text-left">Personal
                         Data
 								 </h5>
                     </div>
-                    <div class="data-box__content-box">
-                        <form action="#" class="form">
-                            <div class="form__container form__container--change-personal-data">
-                                <div class="input-group input-group--change-personal-data">
-                                    <label for="change-personal-data-first-name" class="input-group__label">First
+                    <div className="data-box__content-box">
+                        <form action="#" className="form">
+                            <div className="form__container form__container--change-personal-data">
+                                <div className="input-group input-group--change-personal-data">
+                                    <label for="change-personal-data-first-name" className="input-group__label">First
 											 name:</label>
                                     <input type="text" id='change-personal-data-first-name'
-                                        class="input-group__input input-group__input--invalid" />
-                                    <div class="input-group__round-buttons-container">
-                                        <button class="button-personal-data button-personal-data--edit">
-                                            <svg class="button-personal-data__icon">
+                                        className="input-group__input input-group__input--invalid" />
+                                    <div className="input-group__round-buttons-container">
+                                        <button className="button-personal-data button-personal-data--edit">
+                                            <svg className="button-personal-data__icon">
                                                 <use xlinkHref='/img/sprite.svg#icon-edit' />
                                             </svg>
                                         </button>
                                     </div>
                                 </div>
-                                <div class="input-group input-group--change-personal-data">
+                                <div className="input-group input-group--change-personal-data">
                                     <label for="change-personal-data-first-name"
-                                        class="input-group__label">Email:</label>
+                                        className="input-group__label">Email:</label>
                                     <input type="text" id='change-personal-data-first-name'
-                                        class="input-group__input input-group__input--valid" />
-                                    <div class="input-group__round-buttons-container">
-                                        <button class="button-personal-data button-personal-data--edit">
+                                        className="input-group__input input-group__input--valid" />
+                                    <div className="input-group__round-buttons-container">
+                                        <button className="button-personal-data button-personal-data--edit">
                                         </button>
                                     </div>
                                 </div>
-                                <div class="input-group input-group--change-personal-data">
+                                <div className="input-group input-group--change-personal-data">
                                     <label for="change-personal-data-first-name"
-                                        class="input-group__label">Address:</label>
+                                        className="input-group__label">Address:</label>
                                     <input type="text" id='change-personal-data-first-name'
-                                        class="input-group__input input-group__input" />
-                                    <div class="input-group__round-buttons-container">
-                                        <button class="button-personal-data button-personal-data--abort">
-                                            <svg class="button-personal-data__icon">
+                                        className="input-group__input input-group__input" />
+                                    <div className="input-group__round-buttons-container">
+                                        <button className="button-personal-data button-personal-data--abort">
+                                            <svg className="button-personal-data__icon">
                                                 <use xlinkHref='/img/sprite.svg#icon-cross' />
                                             </svg>
                                         </button>
-                                        <button class="button-personal-data button-personal-data--accept">
-                                            <svg class="button-personal-data__icon">
+                                        <button className="button-personal-data button-personal-data--accept">
+                                            <svg className="button-personal-data__icon">
                                                 <use xlinkHref='/img/sprite.svg#icon-checkmark' />
                                             </svg>
                                         </button>
                                     </div>
                                 </div>
-                                <div class="input-group input-group--change-personal-data">
+                                <div className="input-group input-group--change-personal-data">
                                     <label for="change-personal-data-first-name"
-                                        class="input-group__label">Country:</label>
+                                        className="input-group__label">Country:</label>
                                     <input type="text" id='change-personal-data-first-name'
-                                        class="input-group__input input-group__input--invalid" />
-                                    <div class="input-group__round-buttons-container">
-                                        <button class="button-personal-data button-personal-data--edit">
+                                        className="input-group__input input-group__input--invalid" />
+                                    <div className="input-group__round-buttons-container">
+                                        <button className="button-personal-data button-personal-data--edit">
                                         </button>
                                     </div>
                                 </div>
