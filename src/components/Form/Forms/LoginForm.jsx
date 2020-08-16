31cf038dc6cb0d0966300047f2377e3d20fcc6ec
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-16 08:52:03 +0200

    create and pass 'setResponse' callback

--- src/components/Form/Forms/LoginForm.jsx
+++ src/components/Form/Forms/LoginForm.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import Form from './Form'
 
 export default function LoginForm() {
 
+    const setResponse = useCallback(response => {
+        console.log(response)
+    }, [])
+
     return (
-        <Form name='login' formModifiers='center-column' containerModifiers='auth' title='sign in' buttonText='sign in' children>
+        <Form setResponse={setResponse} name='login' formModifiers='center-column' containerModifiers='auth' title='sign in' buttonText='sign in' children>
             <a href="#" class="form__forgot-password-link">Forgot your password?</a>
         </Form>
     )
--- src/components/Form/Forms/RegisterForm.jsx
+++ src/components/Form/Forms/RegisterForm.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import Form from './Form'
 
 export default function LoginForm() {
 
+    const setResponse = useCallback(response => {
+        console.log(response)
+    }, [])
+
     return (
-        <Form name='register' formModifiers='center-column' containerModifiers='auth' title='create account' buttonText='sign up' />
+        <Form setResponse={setResponse} name='register' formModifiers='center-column' containerModifiers='auth' title='create account' buttonText='sign up' />
     )
 }
