bf5700cc3f3e3aeb8695a2a17492dacffd297eaa
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-16 11:15:09 +0200

    set user after getting response

--- src/components/Form/Forms/LoginForm.jsx
+++ src/components/Form/Forms/LoginForm.jsx
@@ -1,12 +1,17 @@
 import React, { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { setCurrentUser } from '../../../redux/user/actions'
 
 import Form from './Form'
 
 export default function LoginForm() {
+    const dispatch = useDispatch()
+    const history = useHistory()
 
     const setResponse = useCallback(response => {
-        console.log(response)
-    }, [])
+        dispatch(setCurrentUser(response.user))
+    }, [dispatch])
 
     return (
         <Form setResponse={setResponse} name='login' formModifiers='center-column' containerModifiers='auth' title='sign in' buttonText='sign in' children>
