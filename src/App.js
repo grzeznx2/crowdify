09048b9c0021aaf41509ed8015a2f2e97afba069
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-16 14:12:26 +0200

    pass user prop to Header, add route to 'Logout' component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Redirect } from 'react-router-dom'
 
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Logout from './components/Logout/Logout'
 import AuthPage from './pages/AuthPage/AuthPage'
 import DashboardPage from './pages/DashboardPage/DashboardPage'
 import HomePage from './pages/HomePage/HomePage'
@@ -16,7 +17,7 @@ function App() {
   return (
     <div>
       <div className="header-placeholder"></div>
-      <Header />
+      <Header user={user} />
       <Route exact path='/' component={HomePage} />
       <Route exact path='/auth'>
         {user ? <Redirect to='/dashboard/overview' /> : <AuthPage />}
@@ -26,6 +27,7 @@ function App() {
       </Route>
       <Route exact path='/projects/:projectId' component={ProjectPage} />
       <Route exact path='/projects' component={ProjectsPage} />
+      <Route exact path='/logout' component={Logout} />
       <Footer />
     </div>
   );
