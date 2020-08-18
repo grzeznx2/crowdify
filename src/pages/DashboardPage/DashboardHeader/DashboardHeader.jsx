60de26fa11fc6ff20152500c487d912336bd98aa
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 08:33:46 +0200

    add styles, receive 'title' prop

--- src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
+++ src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-// import './DashboardHeader.scss'
+import './DashboardHeader.scss'
 
-export default function DashboardHeader() {
+export default function DashboardHeader({ title }) {
     return (
         <header class="dashboard-header section-dashboard__header">
-            <h3 class="heading-3 dashboard-header__title">Overview</h3>
+            <h3 class="heading-3 dashboard-header__title">{title}</h3>
             <div class="dashboard-header__buttons-container">
                 <a href="#" class="button button--primary">Add Funds</a>
                 <a href="#" class="button button--primary-inverse">Withdraw</a>
