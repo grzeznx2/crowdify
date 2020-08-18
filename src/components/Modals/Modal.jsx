9524a8d50afdfdb83cee5a9069830d9e942ad540
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 09:30:49 +0200

    import and use CloseButton component

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import CloseButton from '../Button/CloseButton/CloseButton'
+
 import { joinClasses } from '../../utils/utils'
 
 import './Modal.scss'
@@ -13,6 +15,7 @@ export default function Modal({ isModalOpen, handleClick, outerContainerModifier
         <div onClick={handleClick} className='modal__background'>
         </div>
         <div className={joinClasses('modal__outer-container', outerContainerModifiers)}>
+            <CloseButton otherClasses='modal__button-close' handleClick={handleClick} iconModifiers='hv-fill-primary' />
             <div className="modal__inner-container">
                 <div className={joinClasses("modal__content", contentModifiers)}>
                     {title && <h3 class="modal__title heading-3 text-white">Withdraw</h3>}
