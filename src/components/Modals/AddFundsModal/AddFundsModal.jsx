a8642403f0aa997d7c9889009ff17ea4bc1f6a0d
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 12:08:41 +0200

    import and use AddFundsForm

--- src/components/Modals/AddFundsModal/AddFundsModal.jsx
+++ src/components/Modals/AddFundsModal/AddFundsModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import AddFundsForm from '../../Form/Forms/AddFundsForm'
 import Modal from '../Modal'
 
 export default function AddFundsModal({ isModalOpen, handleClick }) {
@@ -11,21 +12,7 @@ export default function AddFundsModal({ isModalOpen, handleClick }) {
             outerContainerModifiers='bg-blue'
             contentModifiers='center'
         >
-            <form action="#" class="form">
-                <div class="form__container form__container--center-content">
-                    <div class="input-group input-group--column input-group--on-dark-bg">
-                        <label for="withdraw-from-investment" class="input-group__label">Amount you would like to
-                           withdraw:</label>
-                        <input type="number" id='withdraw-from-investment'
-                            class="input-group__input input-group__input--modal" />
-                        <span class="input-group__additional-info">od 1025$ invested.</span>
-                    </div>
-                </div>
-                <div class="form__buttons-wrapper">
-                    <button class="button button--secondary">Cancel</button>
-                    <button class="button button--secondary">Withdraw</button>
-                </div>
-            </form>
+            <AddFundsForm handleClick={handleClick} />
         </Modal>
     )
 }
