a7e897936837f174753143600064245282df5c7e
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 09:00:49 +0200

    use AddFunsModal

--- src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
+++ src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import AddFundsModal from '../../../components/Modals/AddFundsModal/AddFundsModal'
+
 import './DashboardHeader.scss'
 
 export default function DashboardHeader({ title }) {
     return (
-        <header class="dashboard-header section-dashboard__header">
-            <h3 class="heading-3 dashboard-header__title">{title}</h3>
-            <div class="dashboard-header__buttons-container">
-                <a href="#" class="button button--primary">Add Funds</a>
-                <a href="#" class="button button--primary-inverse">Withdraw</a>
-            </div>
-        </header>
+        <>
+            <AddFundsModal />
+            <header class="dashboard-header section-dashboard__header">
+                <h3 class="heading-3 dashboard-header__title">{title}</h3>
+                <div class="dashboard-header__buttons-container">
+                    <a href="#" class="button button--primary">Add Funds</a>
+                    <a href="#" class="button button--primary-inverse">Withdraw</a>
+                </div>
+            </header>
+        </>
     )
 }
