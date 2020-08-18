c0a0a545197c4a7770d3cf3ca535b56630567b5e
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 09:20:31 +0200

    add open/close AddFundsModal functionality

--- src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
+++ src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
@@ -7,13 +7,17 @@ import './DashboardHeader.scss'
 
 export default function DashboardHeader({ title }) {
     const [isAddFundsModalOpen, setIsAddFundsModalOpen] = useState(false)
+
+    const openAddFundsModal = () => setIsAddFundsModalOpen(true)
+    const closeAddFundsModal = () => setIsAddFundsModalOpen(false)
+
     return (
         <>
-            <AddFundsModal isModalOpen={isAddFundsModalOpen} />
+            <AddFundsModal isModalOpen={isAddFundsModalOpen} handleClick={closeAddFundsModal} />
             <header class="dashboard-header section-dashboard__header">
                 <h3 class="heading-3 dashboard-header__title">{title}</h3>
                 <div class="dashboard-header__buttons-container">
-                    <Button modifiers='primary'>add funds</Button>
+                    <Button handleClick={openAddFundsModal} modifiers='primary'>add funds</Button>
                     <Button modifiers='primary'>withdraw</Button>
                 </div>
             </header>
