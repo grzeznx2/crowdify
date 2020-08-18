4b6c5fb50c0d2bd5793cb6d275e1f501f279a72e
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 12:31:14 +0200

    create WithdrawFundsModal component

---
+++ src/components/Modals/WithdrawFundsModal/WithdrawFundsModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import WithdrawFundsForm from '../../Form/Forms/WithdrawFundsForm'
+import Modal from '../Modal'
+
+export default function WithdrawFundsModal({ isModalOpen, handleClick }) {
+    return (
+        <Modal
+            isModalOpen={isModalOpen}
+            handleClick={handleClick}
+            title='Withdraw funds'
+            outerContainerModifiers='bg-red'
+            contentModifiers='center'
+            text='In case of withdrawal from the investment, a 5% fee will be charged!'
+        >
+            <WithdrawFundsForm handleClick={handleClick} />
+        </Modal>
+    )
+}
