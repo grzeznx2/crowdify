168349a6b482a4aa7c250da7ab391f75564ebacf
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-18 09:16:54 +0200

    import and use Button

--- src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
+++ src/pages/DashboardPage/DashboardHeader/DashboardHeader.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+import Button from '../../../components/Button/Button'
 import AddFundsModal from '../../../components/Modals/AddFundsModal/AddFundsModal'
 
 import './DashboardHeader.scss'
@@ -12,8 +13,8 @@ export default function DashboardHeader({ title }) {
             <header class="dashboard-header section-dashboard__header">
                 <h3 class="heading-3 dashboard-header__title">{title}</h3>
                 <div class="dashboard-header__buttons-container">
-                    <a href="#" class="button button--primary">Add Funds</a>
-                    <a href="#" class="button button--primary-inverse">Withdraw</a>
+                    <Button modifiers='primary'>add funds</Button>
+                    <Button modifiers='primary'>withdraw</Button>
                 </div>
             </header>
         </>
