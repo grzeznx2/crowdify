3b55df7dda506a57f0d2a5c5249157fd46e571ef
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-19 13:43:46 +0200

    replace 'class' with 'className'

--- src/pages/DashboardPage/Tables/Table.jsx
+++ src/pages/DashboardPage/Tables/Table.jsx
@@ -4,58 +4,58 @@ import './Table.scss'
 
 export default function Table() {
     return (
-        <table class="table">
-            <thead class="table__head">
-                <tr class="table__row">
-                    <th class="table__head-cell table__head-cell--date">
+        < table className="table" >
+            <thead className="table__head">
+                <tr className="table__row">
+                    <th className="table__head-cell table__head-cell--date">
                         Date
                         </th>
-                    <th class="table__head-cell table__head-cell--details">
+                    <th className="table__head-cell table__head-cell--details">
                         Details
                         </th>
-                    <th class="table__head-cell table__head-cell--amount">
+                    <th className="table__head-cell table__head-cell--amount">
                         Amount
                         </th>
                 </tr>
             </thead>
-            <tbody class="table__body">
-                <tr class="table__row">
-                    <td class="table__cell">
-                        <span class="table__date">2020-07-24</span>
-                        <span class="table__time">18:21</span>
+            <tbody className="table__body">
+                <tr className="table__row">
+                    <td className="table__cell">
+                        <span className="table__date">2020-07-24</span>
+                        <span className="table__time">18:21</span>
                     </td>
-                    <td class="table__cell">
-                        <span class="table__detail-title">Investment in project</span>
-                        <div class="table__detail-content">
-                            <span class="table__detail-label">Project:</span>
-                            <a href="#" class="table__project-id">CRP-1245</a>
+                    <td className="table__cell">
+                        <span className="table__detail-title">Investment in project</span>
+                        <div className="table__detail-content">
+                            <span className="table__detail-label">Project:</span>
+                            <a href="#" className="table__project-id">CRP-1245</a>
                         </div>
                     </td>
-                    <td class="table__cell table__cell--amount">
-                        <span class="table__amount">55$</span>
+                    <td className="table__cell table__cell--amount">
+                        <span className="table__amount">55$</span>
                     </td>
                 </tr>
-                <tr class="table__row">
-                    <td class="table__cell">
-                        <div class="table__date-container">
-                            <span class="table__date">2020-07-24</span>
-                            <span class="table__time">18:21</span>
+                <tr className="table__row">
+                    <td className="table__cell">
+                        <div className="table__date-container">
+                            <span className="table__date">2020-07-24</span>
+                            <span className="table__time">18:21</span>
                         </div>
                     </td>
-                    <td class="table__cell">
-                        <div class="table__details-container">
-                            <span class="table__detail-title">Investment in project</span>
-                            <div class="table__detail-content">
-                                <span class="table__detail-label">Project:</span>
-                                <a href="#" class="table__project-id">CRP-1245</a>
+                    <td className="table__cell">
+                        <div className="table__details-container">
+                            <span className="table__detail-title">Investment in project</span>
+                            <div className="table__detail-content">
+                                <span className="table__detail-label">Project:</span>
+                                <a href="#" className="table__project-id">CRP-1245</a>
                             </div>
                         </div>
                     </td>
-                    <td class="table__cell table__cell--amount">
-                        <span class="table__amount">55$</span>
+                    <td className="table__cell table__cell--amount">
+                        <span className="table__amount">55$</span>
                     </td>
                 </tr>
             </tbody>
-        </table>
+        </ table>
     )
 }
