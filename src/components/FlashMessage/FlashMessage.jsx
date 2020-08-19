dac3164b7b25e30bf7083b6cde22e0224b3a319c
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-19 08:43:42 +0200

    unmount after 5s, close after clicking on CloseButton

--- src/components/FlashMessage/FlashMessage.jsx
+++ src/components/FlashMessage/FlashMessage.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import { useDispatch } from 'react-redux'
 
 import CloseButton from '../Button/CloseButton/CloseButton'
+
+import { clearFlashMessage } from '../../redux/flashMessage/actions'
 
 import { joinClasses } from '../../utils/utils'
 
 import './FlashMessage.scss'
 
 export default function FlashMessage({ text, messageType }) {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        let mounted = true
+
+        setTimeout(() => {
+            if (mounted) dispatch(clearFlashMessage())
+        }, 5000)
+
+        return () => mounted = false
+    }, [dispatch])
+
+    const closeFlashMessage = () => dispatch(clearFlashMessage())
 
     const flashMessageContainer = document.getElementById('flash-message-root')
+
     const flashMessageMarkup =
         <div className={joinClasses('flash-message', messageType)}>
             <div className="flash-message__content">
@@ -17,13 +34,10 @@ export default function FlashMessage({ text, messageType }) {
             </div>
             <div className="flash-message__close-button-container">
                 <CloseButton
-                    modifiers='' />
+                    handleClick={closeFlashMessage}
+                />
             </div>
         </div>
-
-
-
-
 
     return ReactDOM.createPortal(flashMessageMarkup, flashMessageContainer)
 
