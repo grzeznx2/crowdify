50c9feda339482777f34e19a65c299e25ff93d48
Grzegorz Kubik <grzeznx2@gmail.com>
2020-08-19 10:18:39 +0200

    refactor to use Circle class

--- src/components/Projects/CircleBar/CircleBar.jsx
+++ src/components/Projects/CircleBar/CircleBar.jsx
@@ -1,24 +1,58 @@
 import React from 'react'
 
+import Circle from './Circle'
 import { joinClasses } from '../../../utils/utils'
 
 import './CircleBar.scss'
 
 export default function CircleBar({ paid, minTarget, totalTarget, modifiers, otherClasses }) {
+    const circleCenter = 90
+
     const calcRoundedPercent = (nominator, denominator) => Math.floor((nominator / denominator) * 100)
+
     let minPercent = calcRoundedPercent(paid, minTarget)
+
     minPercent = minPercent > 100 ? 100 : minPercent
+
     const totalPercent = calcRoundedPercent(paid, totalTarget)
+
+    const totalProgressCircle = new Circle(85)
+    const minProgressCircle = new Circle(72)
+
+    totalProgressCircle.calcProgressDashoffset(totalPercent)
+    minProgressCircle.calcProgressDashoffset(minPercent)
 
     return (
         <div className={joinClasses('circle-bar', modifiers, otherClasses)}>
             <svg className="circle-bar__rings">
-                <circle className="circle-bar__ring-bg" cx='90' cy='90' r='85' />
-                <circle className="circle-bar__ring-progress circle-bar__ring-progress--total" cx='90'
-                    cy='90' r='85' />
-                <circle className="circle-bar__ring-bg" cx='90' cy='90' r='72' />
-                <circle className="circle-bar__ring-progress circle-bar__ring-progress--min" cx='90'
-                    cy='90' r='72' />
+                <circle
+                    className="circle-bar__ring-bg"
+                    cx={circleCenter}
+                    cy={circleCenter}
+                    r={totalProgressCircle.radius} />
+                <circle
+                    style={{
+                        strokeDashoffset: totalProgressCircle.progressDashoffset,
+                        strokeDasharray: totalProgressCircle.circumference,
+                    }}
+                    className="circle-bar__ring-progress circle-bar__ring-progress--total"
+                    cx={circleCenter}
+                    cy={circleCenter}
+                    r={totalProgressCircle.radius} />
+                <circle
+                    className="circle-bar__ring-bg"
+                    cx={circleCenter}
+                    cy={circleCenter}
+                    r={minProgressCircle.radius} />
+                <circle
+                    style={{
+                        strokeDashoffset: minProgressCircle.progressDashoffset,
+                        strokeDasharray: minProgressCircle.circumference,
+                    }}
+                    className="circle-bar__ring-progress circle-bar__ring-progress--min"
+                    cx={circleCenter}
+                    cy={circleCenter}
+                    r={minProgressCircle.radius} />
             </svg>
             <div className="circle-bar__percent-box">
                 <span className="circle-bar__percent">{minPercent}%</span>
