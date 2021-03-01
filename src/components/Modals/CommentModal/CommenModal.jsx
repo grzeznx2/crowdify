bf6c5ce0e9e88a3c794d6e2b44b0b598edecd205
grzeznx2 <grzeznx2@gmail.com>
2021-03-01 13:36:50 +0100

    fix component name

--- src/components/Modals/CommentModal/CommenModal.jsx
+++ src/components/Modals/CommentModal/CommenModal.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import AddFundsForm from '../../Form/Forms/CommentForm'
+import CommentForm from '../../Form/Forms/CommentForm'
 import Modal from '../Modal'
 
 export default function CommentModal({ isModalOpen, closeModal }) {
   return (
     <Modal isModalOpen={isModalOpen} closeModal={closeModal} title="Leave a comment" outerContainerModifiers="bg-blue" contentModifiers="center">
-      <AddFundsForm closeModal={closeModal} />
+      <CommentForm closeModal={closeModal} />
     </Modal>
   )
 }
