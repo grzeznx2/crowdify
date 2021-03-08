0fd886f1eb56168bae922894b62e9ae5c57c948f
grzeznx2 <grzeznx2@gmail.com>
2021-03-08 15:28:36 +0100

    add useReducer & refactor

--- src/pages/ProjectPage/Comments/Comments.jsx
+++ src/pages/ProjectPage/Comments/Comments.jsx
@@ -1,93 +1,105 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState, useReducer } from 'react'
+import { useSelector } from 'react-redux'
 import { useHistory, useLocation } from 'react-router-dom'
-import { setParentCommentId, clearParentCommentId, editComment } from '../../../redux/project/actions'
 
 import Button from '../../../components/Button/Button'
 import Comment from './Comment'
 import CommentModal from '../../../components/Modals/CommentModal/CommenModal'
 
+const initialState = {
+  formName: '',
+  modalTitle: '',
+  parentCommentId: '',
+  currentCommentId: '',
+  currentCommentContent: '',
+}
+
+const types = {
+  SET_RESPOND_TO_COMMENT_FORM: 'SET_RESPOND_TO_COMMENT_FORM',
+  SET_EDIT_COMMENT_FORM: 'SET_EDIT_COMMENT_FORM',
+  SET_DELETE_COMMENT_FORM: 'SET_DELETE_COMMENT_FORM',
+  SET_ADD_COMMENT_FORM: 'SET_ADD_COMMENT_FORM',
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.SET_RESPOND_TO_COMMENT_FORM:
+      return {
+        ...state,
+        formName: 'respondToComment',
+        modalTitle: 'respond to the comment',
+        parentCommentId: action.payload.parentCommentId,
+        currentCommentId: '',
+        currentCommentContent: '',
+      }
+    case types.SET_EDIT_COMMENT_FORM:
+      return {
+        ...state,
+        formName: 'editComment',
+        modalTitle: 'edit comment',
+        parentCommentId: '',
+        currentCommentId: action.payload.currentCommentId,
+        currentCommentContent: action.payload.currentCommentContent,
+      }
+    case types.SET_DELETE_COMMENT_FORM:
+      return {
+        ...state,
+        formName: 'deleteComment',
+        modalTitle: 'are you sure you want to delete the comment?',
+        parentCommentId: '',
+        currentCommentId: action.payload.currentCommentId,
+        currentCommentContent: '',
+      }
+    case types.SET_ADD_COMMENT_FORM:
+      return {
+        ...state,
+        formName: 'addComment',
+        modalTitle: 'leave a comment',
+        parentCommentId: '',
+        currentCommentId: '',
+        currentCommentContent: '',
+      }
+    default:
+      return state
+  }
+}
+
 export default function Comments({ comments }) {
-  const [formName, setFormName] = useState('')
-  const [modalTitle, setModalTitle] = useState('')
-  const [parentCommentId, setParentCommentId] = useState('')
-  const [currentCommentId, setCurrentCommentId] = useState('')
-  const [currentCommentContent, setCurrentCommentContent] = useState('')
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const history = useHistory()
   const location = useLocation()
-  const dispatch = useDispatch()
   const user = useSelector(state => state.user.currentUser)
   const [isCommentModalOpen, setIsCommentModalOpen] = useState(false)
 
   const openCommentModal = () => setIsCommentModalOpen(true)
   const closeCommentModal = () => setIsCommentModalOpen(false)
 
-  const handleResponseButton = commentId => {
+  const handleButton = (type, payload) => {
     if (user) {
-      setFormName('respondToComment')
-      setParentCommentId(commentId)
-      setCurrentCommentId('')
-      setCurrentCommentContent('')
-      setModalTitle('respond to the comment')
-      // dispatch(setParentCommentId(commentId))
+      dispatch({ type, payload })
       openCommentModal()
     } else {
       history.push(`/auth?redirectTo=${location.pathname}`)
     }
   }
 
-  const handleEditButton = (commentId, currentCommentContent) => {
-    if (user) {
-      setFormName('editComment')
-      setParentCommentId('')
-      setCurrentCommentId(commentId)
-      setCurrentCommentContent(currentCommentContent)
-      setModalTitle('edit comment')
-      // dispatch(editComment(commentId))
-      openCommentModal()
-    } else {
-      history.push(`/auth?redirectTo=${location.pathname}`)
-    }
-  }
-
-  const handleDeleteButton = commentId => {
-    if (user) {
-      setFormName('deleteComment')
-      setParentCommentId('')
-      setCurrentCommentId(commentId)
-      setCurrentCommentContent('')
-      setModalTitle('are you sure you want to delete the comment?')
-      openCommentModal()
-    } else {
-      history.push(`/auth?redirectTo=${location.pathname}`)
-    }
-  }
-
-  const handleAddCommentButton = () => {
-    if (user) {
-      setFormName('addComment')
-      setParentCommentId('')
-      setCurrentCommentId('')
-      setCurrentCommentContent('')
-      setModalTitle('leave a comment')
-      openCommentModal()
-    } else {
-      history.push(`/auth?redirectTo=${location.pathname}`)
-    }
-  }
+  const handleResponseButton = parentCommentId => handleButton(types.SET_RESPOND_TO_COMMENT_FORM, { parentCommentId })
+  const handleEditButton = (currentCommentId, currentCommentContent) => handleButton(types.SET_EDIT_COMMENT_FORM, { currentCommentId, currentCommentContent })
+  const handleDeleteButton = currentCommentId => handleButton(types.SET_DELETE_COMMENT_FORM, { currentCommentId })
+  const handleAddCommentButton = () => handleButton(types.SET_ADD_COMMENT_FORM)
 
   return (
     <>
       <CommentModal
-        outerContainerModifiers={formName !== 'deleteComment' ? 'bg-blue' : 'bg-red'}
+        outerContainerModifiers={state.formName !== 'deleteComment' ? 'bg-blue' : 'bg-red'}
         isModalOpen={isCommentModalOpen}
         closeModal={closeCommentModal}
-        formName={formName}
-        modalTitle={modalTitle}
-        currentCommentId={currentCommentId}
-        parentCommentId={parentCommentId}
-        currentCommentContent={currentCommentContent}
+        formName={state.formName}
+        modalTitle={state.modalTitle}
+        currentCommentId={state.currentCommentId}
+        parentCommentId={state.parentCommentId}
+        currentCommentContent={state.currentCommentContent}
       />
       <section class="section-comments">
         <h2 class="section-title section-title--text-primary">Comments</h2>
