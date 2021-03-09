71a16039fa2c728bc3e415c46c70b4691f639d44
grzeznx2 <grzeznx2@gmail.com>
2021-03-09 11:54:01 +0100

    add basic rating comments functionality

--- src/pages/ProjectPage/Comments/Comment.jsx
+++ src/pages/ProjectPage/Comments/Comment.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import useFetch from '../../../hooks/useFetch'
 
 import Avatar from '../../../components/Avatar/Avatar'
 import Button from '../../../components/Button/Button'
@@ -10,12 +11,80 @@ const Comment = ({
     content,
     user: { firstName, lastName, photo, id: commentCreatorId },
     depth,
+    rates,
+    id: commentId,
   },
   currentUser,
   handleResponseButton,
   handleEditButton,
   handleDeleteButton,
 }) => {
+  const { isLoading, error, sendRequest } = useFetch()
+
+  let positive = 0
+  let negative = 0
+  let voted = ''
+
+  let rateID = ''
+
+  const handleVotedButton = async isPositive => {
+    let options
+    const possibleOptions = {
+      post: {
+        url: `/api/v1/comments/${commentId}/rates`,
+        body: { isPositive },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      },
+      delete: {
+        url: `/api/v1/rates/${rateID}`,
+        body: null,
+        method: 'DELETE',
+      },
+      update: {
+        url: `/api/v1/rates/${rateID}`,
+        body: { isPositive },
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+      },
+    }
+    if (!rateID) {
+      options = possibleOptions.post
+    }
+    if (isPositive) {
+      if (voted === 'up') {
+        options = possibleOptions.delete
+      }
+
+      if (voted === 'down') {
+        options = possibleOptions.update
+      }
+    } else {
+      if (voted === 'up') {
+        options = possibleOptions.update
+      }
+      if (voted === 'down') {
+        options = possibleOptions.delete
+      }
+    }
+    await sendRequest(options)
+  }
+
+  const handleVoteUpButton = () => handleVotedButton(true)
+  const handleVoteDownButton = () => handleVotedButton(false)
+
+  if (rates) {
+    for (let rate of rates) {
+      if (rate.user === currentUser.id) {
+        rateID = rate._id
+        if (rate.isPositive) voted = 'up'
+        if (!rate.isPositive) voted = 'down'
+      }
+      if (rate.isPositive) positive++
+      else negative++
+    }
+  }
+
   return (
     <div className="comment" style={{ marginLeft: `${(depth - 1) * 50}px` }}>
       <div className="comment__top">
@@ -45,6 +114,18 @@ const Comment = ({
           <Button className="button" handleClick={handleDeleteButton}>
             Delete
           </Button>
+        )}
+        {currentUser && currentUser.id !== commentCreatorId && (
+          <>
+            <Button className="button" handleClick={handleVoteUpButton}>
+              Up
+              <span>{positive}</span>
+            </Button>
+            <Button className="button" handleClick={handleVoteDownButton}>
+              Down
+              <span>{negative}</span>
+            </Button>
+          </>
         )}
       </div>
     </div>
