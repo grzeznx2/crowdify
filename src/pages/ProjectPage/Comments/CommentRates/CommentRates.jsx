0ec8dedebd3071d927c6a55a42e10918dddd85b7
grzeznx2 <grzeznx2@gmail.com>
2021-03-10 12:43:08 +0100

    refactor to use redux instead of useFetch hook

--- src/pages/ProjectPage/Comments/CommentRates/CommentRates.jsx
+++ src/pages/ProjectPage/Comments/CommentRates/CommentRates.jsx
@@ -1,48 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import useFetch from '../../../../hooks/useFetch'
+import { useSelector, useDispatch } from 'react-redux'
+import { postCommentRate, updateCommentRate, deleteCommentRate } from '../../../../redux/commentsRates/actions'
 
 import Button from '../../../../components/Button/Button'
 
 const CommentRates = ({ commentId, currentUserId }) => {
-  const { isLoading, error, sendRequest } = useFetch()
+  const dispatch = useDispatch()
   const commentRates = useSelector(state => state.commentsRates.byCommentId[commentId])
-  let positive = 0
-  let negative = 0
+
+  let positiveVotesCount = 0
+  let negativeVotesCount = 0
   let voted = ''
-  let rateID = ''
+  let rateId = ''
 
   const handleVotedButton = async isPositive => {
-    let options
-    const possibleOptions = {
-      post: {
-        url: `/api/v1/comments/${commentId}/rates`,
-        body: { isPositive },
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      },
-      delete: {
-        url: `/api/v1/rates/${rateID}`,
-        body: null,
-        method: 'DELETE',
-      },
-      update: {
-        url: `/api/v1/rates/${rateID}`,
-        body: { isPositive },
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-      },
-    }
-    if (!rateID) {
-      options = possibleOptions.post
-    } else if (isPositive) {
-      if (voted === 'up') options = possibleOptions.delete
-      else if (voted === 'down') options = possibleOptions.update
+    if (!rateId) dispatch(postCommentRate(isPositive, commentId))
+    else if (isPositive) {
+      if (voted === 'up') dispatch(deleteCommentRate(rateId, commentId))
+      else if (voted === 'down') dispatch(updateCommentRate(isPositive, rateId))
     } else {
-      if (voted === 'up') options = possibleOptions.update
-      else if (voted === 'down') options = possibleOptions.delete
+      if (voted === 'up') dispatch(updateCommentRate(isPositive, rateId))
+      else if (voted === 'down') dispatch(deleteCommentRate(rateId, commentId))
     }
-    await sendRequest(options)
   }
 
   const handleVoteUpButton = () => handleVotedButton(true)
@@ -50,23 +29,23 @@ const CommentRates = ({ commentId, currentUserId }) => {
 
   for (let rate of commentRates) {
     if (rate.user === currentUserId) {
-      rateID = rate._id
+      rateId = rate._id
       if (rate.isPositive) voted = 'up'
       if (!rate.isPositive) voted = 'down'
     }
-    if (rate.isPositive) positive++
-    else negative++
+    if (rate.isPositive) positiveVotesCount++
+    else negativeVotesCount++
   }
 
   return (
     <>
       <Button className="button" handleClick={handleVoteUpButton}>
         Up
-        <span>{positive}</span>
+        <span>{positiveVotesCount}</span>
       </Button>
       <Button className="button" handleClick={handleVoteDownButton}>
         Down
-        <span>{negative}</span>
+        <span>{negativeVotesCount}</span>
       </Button>
     </>
   )
