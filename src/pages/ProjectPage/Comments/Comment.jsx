d5092f0e069ef84f8ce7932c73f618da07005f73
grzeznx2 <grzeznx2@gmail.com>
2021-03-10 09:03:51 +0100

    use CommenRates component

--- src/pages/ProjectPage/Comments/Comment.jsx
+++ src/pages/ProjectPage/Comments/Comment.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import useFetch from '../../../hooks/useFetch'
 
 import Avatar from '../../../components/Avatar/Avatar'
 import Button from '../../../components/Button/Button'
+import CommentRates from './CommentRates/CommentRates'
 import './Comment.scss'
 
 const Comment = ({
@@ -19,61 +18,6 @@ const Comment = ({
   handleEditButton,
   handleDeleteButton,
 }) => {
-  const { isLoading, error, sendRequest } = useFetch()
-  const commentRates = useSelector(state => state.commentsRates.byCommentId[commentId])
-
-  let positive = 0
-  let negative = 0
-  let voted = ''
-
-  let rateID = ''
-
-  const handleVotedButton = async isPositive => {
-    let options
-    const possibleOptions = {
-      post: {
-        url: `/api/v1/comments/${commentId}/rates`,
-        body: { isPositive },
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      },
-      delete: {
-        url: `/api/v1/rates/${rateID}`,
-        body: null,
-        method: 'DELETE',
-      },
-      update: {
-        url: `/api/v1/rates/${rateID}`,
-        body: { isPositive },
-        method: 'PATCH',
-        headers: { 'Content-Type': 'application/json' },
-      },
-    }
-    if (!rateID) {
-      options = possibleOptions.post
-    } else if (isPositive) {
-      if (voted === 'up') options = possibleOptions.delete
-      else if (voted === 'down') options = possibleOptions.update
-    } else {
-      if (voted === 'up') options = possibleOptions.update
-      else if (voted === 'down') options = possibleOptions.delete
-    }
-    await sendRequest(options)
-  }
-
-  const handleVoteUpButton = () => handleVotedButton(true)
-  const handleVoteDownButton = () => handleVotedButton(false)
-
-  for (let rate of commentRates) {
-    if (rate.user === currentUser.id) {
-      rateID = rate._id
-      if (rate.isPositive) voted = 'up'
-      if (!rate.isPositive) voted = 'down'
-    }
-    if (rate.isPositive) positive++
-    else negative++
-  }
-
   return (
     <div className="comment" style={{ marginLeft: `${(depth - 1) * 50}px` }}>
       <div className="comment__top">
@@ -104,18 +48,7 @@ const Comment = ({
             Delete
           </Button>
         )}
-        {currentUser && currentUser.id !== commentCreatorId && (
-          <>
-            <Button className="button" handleClick={handleVoteUpButton}>
-              Up
-              <span>{positive}</span>
-            </Button>
-            <Button className="button" handleClick={handleVoteDownButton}>
-              Down
-              <span>{negative}</span>
-            </Button>
-          </>
-        )}
+        {currentUser && currentUser.id !== commentCreatorId && <CommentRates commentId={commentId} currentUserId={currentUser.id} />}
       </div>
     </div>
   )
