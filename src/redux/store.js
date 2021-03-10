af6a6088fad82c953d0c8e1a14e0a95e74248e61
grzeznx2 <grzeznx2@gmail.com>
2021-03-10 09:13:33 +0100

    add redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
-
+import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
-export const store = createStore(rootReducer)
-export const persistor = persistStore(store)
+const middlewares = [thunk]
 
-// export default { store, persistor }
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const persistor = persistStore(store)
