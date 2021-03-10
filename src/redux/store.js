208f24f9d32c1dba14aac5640ce06b3104f697bb
grzeznx2 <grzeznx2@gmail.com>
2021-03-10 11:48:08 +0100

    add redux-devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
 const middlewares = [thunk]
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
 export const persistor = persistStore(store)
