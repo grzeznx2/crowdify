81a67445a589a343c9e03aea6a59d452fc994735
grzeznx2 <grzeznx2@gmail.com>
2021-03-11 13:40:56 +0100

    format

--- src/redux/flashMessage/reducer.js
+++ src/redux/flashMessage/reducer.js
@@ -1,28 +1,28 @@
 import types from './types'
 
 const initialState = {
-    mounted: false,
-    text: '',
-    messageType: ''
+  mounted: false,
+  text: '',
+  messageType: '',
 }
 
 export default function flashMessageReducer(state = initialState, action) {
-    switch (action.type) {
-        case types.SET_FLASH_MESSAGE:
-            return {
-                ...state,
-                mounted: true,
-                text: action.text,
-                messageType: action.messageType
-            }
-        case types.CLEAR_FLASH_MESSAGE:
-            return {
-                ...state,
-                mounted: false,
-                text: '',
-                messageType: ''
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case types.SET_FLASH_MESSAGE:
+      return {
+        ...state,
+        mounted: true,
+        text: action.text,
+        messageType: action.messageType,
+      }
+    case types.CLEAR_FLASH_MESSAGE:
+      return {
+        ...state,
+        mounted: false,
+        text: '',
+        messageType: '',
+      }
+    default:
+      return state
+  }
 }
